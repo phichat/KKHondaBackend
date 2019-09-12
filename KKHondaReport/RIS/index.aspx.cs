037afcd3087034cf827c5b662243737829a028c2
phichat <phichat_dk@hotmail.com>
2019-09-12 09:39:57 +0700

    commit

--- KKHondaReport/RIS/index.aspx.cs
+++ KKHondaReport/RIS/index.aspx.cs
@@ -21,14 +21,39 @@ namespace KKHondaReport.RIS
         protected void Page_Load(object sender, EventArgs e)
         {
             var sedNo = string.Empty;
+            var clNo = string.Empty;
+            var alNo = string.Empty;
+            var userId = string.Empty;
             if (Request.QueryString["sedNo"] != null)
             {
                 sedNo = Request.QueryString["sedNo"];
+            }
+            if (Request.QueryString["userId"] != null)
+            {
+                userId = Request.QueryString["userId"];
+            }
+            if (Request.QueryString["clNo"] != null)
+            {
+                clNo = Request.QueryString["clNo"];
+            }
+            if (Request.QueryString["alNo"] != null)
+            {
+                alNo = Request.QueryString["alNo"];
             }
 
             if (Request.QueryString["formSed"] != null)
             {
                 ExportSED(sedNo);
+            }
+
+            if (Request.QueryString["formCl"] != null)
+            {
+                ExportCL(clNo, userId);
+            }
+
+            if (Request.QueryString["formAl"] != null)
+            {
+                ExportAL(alNo, userId);
             }
         }
 
@@ -50,6 +75,60 @@ namespace KKHondaReport.RIS
                 rptDoc.Database.Tables[0].ApplyLogOnInfo(L1);
 
                 StreamPdfReport(rptDoc, "sed-doc.pdf");
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        private void ExportCL(string clNo, string userId)
+        {
+            try
+            {
+                //RIS/index.aspx?alNo=AL026209/0001&userId=4111&formAl=true
+                rptDoc = new ReportDocument();
+                SqlConnectionStringBuilder connection = new SqlConnectionStringBuilder(conStr);
+                var server = connection.DataSource;
+
+                var file = "./RegisCL.rpt";
+                rptDoc.Load(Server.MapPath(file));
+                rptDoc.Refresh();
+
+                TableLogOnInfo L1 = rptDoc.Database.Tables[0].LogOnInfo;
+                GetLoginfo(L1, server);
+                rptDoc.SetParameterValue("@user_id", userId);
+                rptDoc.SetParameterValue("@cl_no", clNo);
+                rptDoc.Database.Tables[0].ApplyLogOnInfo(L1);
+
+                StreamPdfReport(rptDoc, "cl-doc.pdf");
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        private void ExportAL(string alNo, string userId)
+        {
+            try
+            {
+                //RIS/index.aspx?clNo=CL016209/0001&userId=4111&formCl=true
+                rptDoc = new ReportDocument();
+                SqlConnectionStringBuilder connection = new SqlConnectionStringBuilder(conStr);
+                var server = connection.DataSource;
+
+                var file = "./RegisAL.rpt";
+                rptDoc.Load(Server.MapPath(file));
+                rptDoc.Refresh();
+
+                TableLogOnInfo L1 = rptDoc.Database.Tables[0].LogOnInfo;
+                GetLoginfo(L1, server);
+                rptDoc.SetParameterValue("@user_id", userId);
+                rptDoc.SetParameterValue("@al_no", alNo);
+                rptDoc.Database.Tables[0].ApplyLogOnInfo(L1);
+
+                StreamPdfReport(rptDoc, "al-doc.pdf");
             }
             catch (Exception ex)
             {
